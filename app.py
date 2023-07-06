b9e3096c34f4d7f1032b08a7ebd51425b60ec2da
Rick(Rugui) Chen <rickruguichen@google.com>
2023-07-05 21:23:05 -0400

    RE checkout

--- app.py
+++ app.py
@@ -4,24 +4,18 @@ import os
 from backend.property_llm import predict_llm_property
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/submit")
 def index():
     # Get the input from the user
     input_text = request.form.get("input_text")
     option_text = request.form['options']
     
-    # Generate the output text
-    #output_text = "You entered: " + input_text
-    #for option in options:
-    #    output_text += "\nYou selected: " + option
-
-    # Render the template
-
-
-
     output_text=predict_llm_property(input_text)
     return render_template("index.html", input_text=input_text, options=option_text, output_text=output_text)
 
+@app.route("/")
+def index():
+    return render_template("index.html", input_text="Enter description:", options='rewrite', output_text='')
 if __name__ == "__main__":
     app.run(debug=True)
 
