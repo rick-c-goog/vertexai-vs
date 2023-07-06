88ee728517ca4324dd1538797aa3684c05eb8f64
Rick(Rugui) Chen <rickruguichen@google.com>
2023-07-06 10:32:59 -0400

    RE checkout

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def submit():
     # Get the input from the user
     input_text = request.form.get("input_text")
-    app.logger.info('%s input text:', input_text)
-    option_text = request.form['options']
     
+    option_text = request.form.get("options")
+    app.logger.info('Checked: %s ', option_text)
     output_text=predict_llm_property(input_text)
     return render_template("index.html", input_text=input_text, options=option_text, output_text=output_text)
 
